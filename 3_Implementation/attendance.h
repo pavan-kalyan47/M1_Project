d1f2a636a03cbfd94a2bb30e2a26a431b6881fe3
pavan-kalyan47 <101034516+pavan-kalyan47@users.noreply.github.com>
2022-04-01 12:34:59 +0530

    Create attendance.h

---
+++ 3_Implementation/attendance.h
@@ -0,0 +1,12 @@
+// Hader Fles
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// Fnctions Declaration
+  
+void checkAttendance();
+void addStudent();
+void removeStudent();
+void takeAttendance()
